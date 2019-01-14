8b684d5c7963a97882e7812de91d2768d94491b2
Meraj khan <khanmeraj83@gmail.com>
2019-01-14 21:39:23 +0500

    Updates

---
+++ precache-manifest.39284ce08d2f2d790022adc7c4044578.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "38258589ee62837be827",
+    "url": "/youtubevideos/static/js/runtime~main.38258589.js"
+  },
+  {
+    "revision": "ef5e745f8cfc8004a79f",
+    "url": "/youtubevideos/static/js/main.ef5e745f.chunk.js"
+  },
+  {
+    "revision": "102c3bf24ee7bc42981e",
+    "url": "/youtubevideos/static/js/1.102c3bf2.chunk.js"
+  },
+  {
+    "revision": "ef5e745f8cfc8004a79f",
+    "url": "/youtubevideos/static/css/main.823b6829.chunk.css"
+  },
+  {
+    "revision": "df1501d021af8105fbc028959b21eb73",
+    "url": "/youtubevideos/index.html"
+  }
+];
